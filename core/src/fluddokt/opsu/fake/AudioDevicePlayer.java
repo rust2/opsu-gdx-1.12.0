f9762b63cd42d02d072a02fdcaec9214010d1ea9
rust2 <rustchannelz@gmail.com>
2023-10-02 20:23:13 +0500

    apply formatting

--- core/src/fluddokt/opsu/fake/AudioDevicePlayer.java
+++ core/src/fluddokt/opsu/fake/AudioDevicePlayer.java
@@ -2,54 +2,41 @@ package fluddokt.opsu.fake;
 
 public abstract class AudioDevicePlayer {
 
-	public abstract void setAudioDeviceListener(AudioDeviceListener audioDeviceListener);
+    public abstract void setAudioDeviceListener(AudioDeviceListener audioDeviceListener);
 
-	public boolean setPosition(float f) {
-		return false;
-	}
+    public boolean setPosition(float f)
+    {
+        return false;
+    }
 
-	public void loop() {
-		
-	}
+    public void loop() {}
 
-	public void play() {
-		
-	}
+    public void play() {}
 
-	public boolean playing() {
-		return false;
-	}
+    public boolean playing()
+    {
+        return false;
+    }
 
-	public void pause() {
-		
-	}
+    public void pause() {}
 
-	public void resume() {
-		
-	}
+    public void resume() {}
 
-	public void setVolume(float volume) {
-		
-	}
+    public void setVolume(float volume) {}
 
-	public void stop() {
-		
-	}
+    public void stop() {}
 
-	public float getPosition() {
-		return 0;
-	}
+    public float getPosition()
+    {
+        return 0;
+    }
 
-	public void dispose() {
-		
-	}
+    public void dispose() {}
 
-	public String getName() {
-		return null;
-	}
+    public String getName()
+    {
+        return null;
+    }
 
-	public void setPitch(float pitch) {
-		
-	}
-
+    public void setPitch(float pitch) {}
 }
